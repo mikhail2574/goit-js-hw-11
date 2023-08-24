6679e8387ba0d8740f88355944c23666e4bbc7d5
mykhailo_naumenko <74735041+mikhail2574@users.noreply.github.com>
2023-08-24 22:46:36 +0200

    debug and style button

--- src/fetch-images.js
+++ src/fetch-images.js
@@ -1,36 +1,30 @@
 // ## Initializing ##
 import axios from 'axios';
+import Notiflix from 'notiflix';
 const BASE_URL = 'https://pixabay.com/api/';
 
-let page = 1;
+const params = new URLSearchParams({
+  q: undefined,
+  key: '38966446-c32fc19d0a971996be7b08c24',
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: true,
+  per_page: 40,
+  page: 1,
+});
 
 export async function getImages(text) {
-  if (!text || text == ' ') {
-    alert("Don't do it!");
+  params.set('q', text.trim());
+  if (!params.get('q')) {
+    Notiflix.Notify.failure("Don't do it!");
     return;
+  } else {
+    params.set('page', 1);
+    return await axios.get(`${BASE_URL}?${params}`);
   }
-  const params = new URLSearchParams({
-    q: text,
-    key: '38966446-c32fc19d0a971996be7b08c24',
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-    per_page: 40,
-    page: page,
-  });
-  return await axios.get(`${BASE_URL}?${params}`);
 }
-export async function getImagesNext(text) {
-  page += 1;
 
-  const params = new URLSearchParams({
-    q: text,
-    key: '38966446-c32fc19d0a971996be7b08c24',
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-    per_page: 40,
-    page: page,
-  });
+export async function getImagesNext() {
+  params.set('page', Number(params.get('page')) + 1);
   return await axios.get(`${BASE_URL}?${params}`);
 }
--- src/index.js
+++ src/index.js
@@ -1,77 +1,97 @@
 // Import
 import { getImages, getImagesNext } from './fetch-images';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import Notiflix from 'notiflix';
 // ## Initialization ##
 let form = document.querySelector('.search-form');
 let gallery = document.querySelector('.gallery');
 let btnLoad = document.querySelector('[js-load]');
 let page = 1;
 
-// Function on search
-form.addEventListener('submit', evt => {
+let onSubmit = async function (evt) {
   evt.preventDefault();
-  setMarkup(getImages(evt.target.searchQuery.value));
-});
+  setMarkup(await getImages(evt.target.searchQuery.value));
+  var lightbox = new SimpleLightbox('.gallery a');
+  lightbox.refresh();
+};
 
-btnLoad.addEventListener('click', () => {
-  setMarkupNext(getImagesNext(document.querySelector("[type='text']").value));
-});
+let onClick = async function (evt) {
+  setMarkupNext(
+    await getImagesNext(document.querySelector("[type='text']").value)
+  );
+};
+
+// Function on search
+form.addEventListener('submit', onSubmit);
+
+btnLoad.addEventListener('click', onClick);
 
 // Setting markup for gallery
-function setMarkup(response) {
-  response
-    .then(images => {
-      const {
-        data: { hits, totalHits },
-      } = images;
-      if (totalHits == 0) {
-        alert('Not found!!!');
-        return;
-      }
-      if (totalHits <= 40 * page) {
-        alert("We're sorry, but you've reached the end of search results.");
-        btnLoad.classList.add('invisible');
-      }
-      gallery.innerHTML = hits
-        .map(image => {
-          return setImage(image);
-        })
-        .join('');
+function setMarkup(images) {
+  if (images == undefined) {
+    return;
+  }
+  const {
+    data: { hits, totalHits },
+  } = images;
+  if (totalHits == 0) {
+    Notiflix.Notify.failure('Not found');
+    return;
+  }
+  Notiflix.Notify.success('Found: ' + totalHits + ' images!');
+  btnLoad.classList.remove('invisible');
+  btnLoad.classList.add('load-btn');
+  if (totalHits <= 40) {
+    btnLoad.classList.add('invisible');
+  }
+  gallery.innerHTML = hits
+    .map(image => {
+      return setImage(image);
     })
-    .catch(error => {
-      throw error;
-    });
+    .join('');
 }
 
-function setMarkupNext(response) {
-  response
-    .then(images => {
-      const {
-        data: { hits, totalHits },
-      } = images;
-      if (totalHits == 0) {
-        console.log('Not found!!!');
-        return;
-      }
-      page += 1;
-      if (totalHits <= 40 * page) {
-        alert("We're sorry, but you've reached the end of search results.");
-        btnLoad.classList.add('invisible');
-      }
-      gallery.innerHTML += hits
-        .map(image => {
-          return setImage(image);
-        })
-        .join('');
+function setMarkupNext(images) {
+  const {
+    data: { hits, totalHits },
+  } = images;
+  if (totalHits == 0) {
+    Notiflix.Notify.failure('Not found');
+    return;
+  }
+  btnLoad.classList.remove('invisible');
+  page += 1;
+  if (totalHits <= 40 * page) {
+    Notiflix.Notify.info(
+      "We're sorry, but you've reached the end of search results."
+    );
+    btnLoad.classList.add('invisible');
+  }
+  gallery.innerHTML += hits
+    .map(image => {
+      return setImage(image);
     })
-    .catch(error => {
-      throw error;
-    });
+    .join('');
 }
 
 function setImage(image) {
-  const { webformatURL, tags, views, likes, comments, downloads } = image;
-  return `<div class="photo-card">
-    <img src="${webformatURL}" alt="${tags}" width="335" height="200" class="image" loading="lazy" />
+  const {
+    largeImageURL,
+    webformatURL,
+    tags,
+    views,
+    likes,
+    comments,
+    downloads,
+  } = image;
+  return `  
+  <div class="photo-card gallery__item">
+  <a href="${largeImageURL}">
+    <img class="gallery__image" src="${webformatURL}" alt="${tags}" width="335" height="200" class="image" loading="lazy" />
+      </a>
+
+    
     <div class="info">
       <p class="info-item">
         <b>Likes</b> ${likes}
@@ -86,5 +106,6 @@ function setImage(image) {
         <b>Downloads</b> ${downloads}
       </p>
     </div>
-  </div>`;
+  </div>
+  `;
 }
